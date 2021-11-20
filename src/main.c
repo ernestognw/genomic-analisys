2e193e076b2f84bfb124f55762b9203694d0967d
Karina Amador <akarina.at@gmail.com>
2021-11-20 16:00:07 -0600

    Co-authored-by: Alan M. <Am3ra@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main() {
+#define BUFF_SIZE 16000000
+
+char source[BUFF_SIZE + 1];
+
+//Returns size of sequence
+size_t readFile()
+{
+   FILE *fp = fopen("S._cerevisiae_processed.txt", "r");
+   if (fp != NULL)
+   {
+      //file stored in memory
+      size_t newLen = fread(source, sizeof(char), BUFF_SIZE, fp);
+      if (ferror(fp) != 0)
+      {
+         fputs("Error reading file", stderr);
+      }
+      else
+      {
+         source[newLen++] = '\0'; /* Just to be safe. */
+      }
+      fclose(fp);
+      return newLen;
+   }
+}
+
+int main()
+{
+
    printf("Hello, World!\n");
 
    return 0;

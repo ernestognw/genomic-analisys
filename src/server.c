1235948201a2114de8275937bf32ba4792a18028
ernestognw <ernestognw@gmail.com>
2021-11-26 14:38:51 -0600

    Feature: Added coverage

--- src/server.c
+++ src/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "constants.c"
 
 char storedReference[BUFF_SIZE];
@@ -64,6 +65,41 @@ void processLine(const char *line, int interval[2])
   }
 }
 
+int compareFirst(const void *a, const void *b)
+{
+  return *(int *)a - *(int *)b;
+}
+
+int intervalsCoverage(int intervals[][2], int intervalsAmount)
+{
+  qsort(intervals, intervalsAmount, sizeof(int *), compareFirst);
+
+  int coverage = 0;
+  int minStart = 0;
+
+  int i = 0;
+  while (i < intervalsAmount)
+  {
+    int start = fmax(minStart, intervals[i][0]);
+    int end = intervals[i][1];
+
+    int advance = 1;
+    for (int j = i + 1; j < intervalsAmount; j++)
+    {
+      if (intervals[j][0] > end)
+        break;
+      end = fmax(end, intervals[j][1]);
+      advance++;
+    }
+
+    i += advance;
+    coverage += fmax(0, end - start);
+    minStart = end;
+  }
+
+  return coverage;
+}
+
 void uploadSequence(char uploadedSequence[])
 {
   if (strlen(storedReference) == 0)
@@ -77,10 +113,9 @@ void uploadSequence(char uploadedSequence[])
     for (int i = 0; *(splitted + i); i++)
       processLine(*(splitted + i), intervals[i]);
 
-    for (int i = 0; i < linesCount; i++)
-    {
-      printf("[%d,%d]\n", intervals[i][0], intervals[i][1]);
-    }
+    int coverage = intervalsCoverage(intervals, linesCount);
+    float percentage = (float)coverage / (float)strlen(storedReference) * 100;
+    printf("%d lines covering %f %% of the genoma reference\n", coverage, percentage);
   }
 }
 

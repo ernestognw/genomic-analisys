91eed9f7146a65c6580f86dfede3c57d7e19629c
ernestognw <ernestognw@gmail.com>
2021-11-26 16:22:13 -0600

    Improvements

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { readFileSync } = require("fs");
 
-const reference = readFileSync("./src/tests/2/ref.seq").toString();
-const sequence = readFileSync("./src/tests/2/seq.txt").toString();
+const reference = readFileSync("./src/tests/1/ref.seq").toString();
+const sequence = readFileSync("./src/tests/1/seq.txt").toString();
 
 sequence.split("\n").forEach((line) => {
   const index = reference.indexOf(line);
   if (index !== -1 && line) {
     console.log(`${line} found at index ${index}`);
+  } else {
+    console.log('Not found')
   }
 });

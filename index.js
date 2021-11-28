854257c6a48b232ffa50df6d45d7320e452e7871
ernestognw <ernestognw@gmail.com>
2021-11-28 17:20:26 -0600

    Feature: Improved JS script comparator

--- index.js
+++ index.js
@@ -3,11 +3,49 @@ const { readFileSync } = require("fs");
 const reference = readFileSync("./src/tests/1/ref.seq").toString();
 const sequence = readFileSync("./src/tests/1/seq.txt").toString();
 
-sequence.split("\n").forEach((line) => {
-  const index = reference.indexOf(line);
-  if (index !== -1 && line) {
-    console.log(`${line} found at index ${index}`);
-  } else {
-    console.log('Not found')
+let included = 0;
+let characters = 0;
+const intervals = [];
+
+const coverage = (ranges) => {
+  if (ranges.length < 2) return ranges;
+
+  ranges.sort((a, b) => a[0] - b[0]);
+
+  const mergedRanges = [];
+  let previous = ranges[0];
+
+  for (let i = 1; i < ranges.length; i += 1) {
+    if (previous[1] >= ranges[i][0]) {
+      previous = [previous[0], Math.max(previous[1], ranges[i][1])];
+    } else {
+      mergedRanges.push(previous);
+      previous = ranges[i];
+    }
+  }
+
+  mergedRanges.push(previous);
+
+  return mergedRanges.reduce((acc, [start, end]) => acc + (end - start), 0);
+};
+
+sequence.split("\n").forEach((line, index) => {
+  const trimmedLine = line.trim();
+  const position = reference.indexOf(trimmedLine);
+
+  if (position !== -1 && line) {
+    console.log(`Line ${index} found at index ${position}`);
+    included += trimmedLine.length;
+    characters++;
+    intervals.push([position, position + trimmedLine.length]);
   }
 });
+
+const calculatedCoverage = coverage(intervals);
+
+console.log(
+  `${calculatedCoverage} characters covering ${(
+    (calculatedCoverage / reference.length) *
+    100
+  ).toFixed(2)}% of the genoma reference`
+);
